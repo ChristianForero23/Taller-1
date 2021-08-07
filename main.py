d6537fb498245e6f13b9bef2dc6ebf647bdbfe29
ChristianForero23 <68068825+ChristianForero23@users.noreply.github.com>
2021-08-07 13:40:11 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Taller0 import *
-
-path=input("ingrese entre comillas la ruta de la fotografia que desea procesar")
-imagen= basicColor(path)
-imagen.displayProperties()
-cv2.imshow("Otsu",imagen.makeBW())
-cv2.imshow(" ",imagen.colorize(100))
-cv2.waitKey(0)
